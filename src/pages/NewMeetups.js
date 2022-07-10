558273b0b482ade6571c89cdc86c09f8af291b16
guneyunus <guney.yunus.dev@outlook.com>
2022-07-11 01:59:54 +0300

    http post with fetch method with Firebase db

--- src/pages/NewMeetups.js
+++ src/pages/NewMeetups.js
@@ -1,5 +1,21 @@
+import NewMeetupForm from "../components/meetup/NewMeetup";
+
 function NewMeetupsPage(){
-    return <div>New MeetupsPage</div>
+   function addMeetupHandler(meetupData){
+    fetch('https://react-getting-started-2adc8-default-rtdb.firebaseio.com/meetups.json'
+    ,{
+        method : 'POST',
+        body : JSON.stringify(meetupData),
+        headers : {
+            'Content-Type' : 'application/json'
+        }
+    }
+    );
+   }
+   
+   return <section>
+        <NewMeetupForm onAddMeetup={addMeetupHandler}></NewMeetupForm>
+    </section>
 }
 
 export default NewMeetupsPage;
