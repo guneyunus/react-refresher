38e6152cb37301ea284cf63235ed16a25c8a0f59
guneyunus <guney.yunus.dev@outlook.com>
2022-07-11 03:12:35 +0300

    Context api setup

---
+++ src/store/favorites-context.js
@@ -0,0 +1,18 @@
+import { createContext ,useState} from "react";
+
+const FavoritesContext = createContext({
+    favorites : [],
+    totalFavorites : 0
+});
+
+function FavoritesContextProvider(props){
+    const [userFavorites,setUserFavorites]= useState([]);
+    const context = {
+        favorites :userFavorites,
+        totalFavorites:userFavorites.length
+    } ;
+
+    return <FavoritesContext.Provider>
+        {props.children}
+    </FavoritesContext.Provider>
+}
